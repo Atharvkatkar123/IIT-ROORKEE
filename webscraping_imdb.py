ef2dfdecdc896861751b55dd1a3948c664e0ebaf
Atharv Katkar <94977707+Atharvkatkar123@users.noreply.github.com>
2025-03-04 23:55:30 +0530

    Update webscraping_imdb.py

--- webscraping_imdb.py
+++ webscraping_imdb.py
@@ -3,7 +3,6 @@ import requests
 from bs4 import BeautifulSoup
 import numpy as np
 import pandas as pd
-#myvery own code
 #much needed beautifulsoup
 
 def download_image(image_url, folder_name, actor_name):
