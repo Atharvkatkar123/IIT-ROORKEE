c13a5d4d444b6221899edce0c21eb89e7a764fc2
Atharv Katkar <94977707+Atharvkatkar123@users.noreply.github.com>
2025-03-27 20:43:09 +0530

    Update webscraping_imdb.py

--- webscraping_imdb.py
+++ webscraping_imdb.py
@@ -3,7 +3,8 @@ import requests
 from bs4 import BeautifulSoup
 import numpy as np
 import pandas as pd
-
+import matplotlib.pyplot as plt
+from sklearn.linear import LinearRegression 
 #much needed beautifulsoup
 
 
