56e660eeff96d8304144bed5cc9027bb0c275e50
Atharv Katkar <94977707+Atharvkatkar123@users.noreply.github.com>
2025-09-02 23:05:39 +0530

    Update webscraping_imdb.py

--- webscraping_imdb.py
+++ webscraping_imdb.py
@@ -5,6 +5,7 @@ import numpy as np
 import pandas as pd
 import matplotlib.pyplot as plt
 #much needed beautifulsoup
+#this is vary commit...!
 def download_image(image_url, folder_name, actor_name):
     #os.makedirs(folder_name, exist_ok=True)  
     save_path = os.path.join(folder_name, f"{actor_name}.jpg")  
